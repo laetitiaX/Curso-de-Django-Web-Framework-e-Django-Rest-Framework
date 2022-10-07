e274db495bc1df8d8d4bfdc90e3e8ee8fea7a219
Letícia Xavier de Oliveira <leticiaoliveiraxavier2@gmail.com>
2022-10-07 17:32:15 -0300

    atualização recipe

--- projeto/urls.py
+++ projeto/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
